20c364b150b19ac984a4d93afb24346de349077a
Armen23023 <mirzoyana805@gmail.com>
2022-12-12 19:11:27 +0400

    WIP

--- src/W1Day1/CodeSignal.java
+++ src/W1Day1/CodeSignal.java
@@ -5,28 +5,33 @@ import java.util.Arrays;
 public class CodeSignal
 {
    static int solution(int n, int k) {
-        String s = Integer.toBinaryString(n);
-        char[] chars = s.toCharArray();
+//        String s = Integer.toBinaryString(n);
+//        char[] chars = s.toCharArray();
+//
+//        if (chars[chars.length-k]==0) return n;
+//        else {
+//            chars[chars.length-k]='0';
+//        }
+//        int result=1;
+//        int arr[]= new int[chars.length];
+//       for (int i = chars.length-2; i >= 0; i--) {
+//           int numericValue = Character.getNumericValue(chars[i]);
+//           result = result * 2;
+//           if (numericValue==1) {
+//               arr[i] =  result;
+//           }
+//           }
+//            int sum = 0;
+//       for (int i = 0; i < arr.length; i++) {
+//            sum =  sum + arr[i] ;
+//       }
+//        return sum+1;
 
-        if (chars[chars.length-k]==0) return n;
-        else {
-            chars[chars.length-k]='0';
-        }
-        int result=1;
-        int arr[]= new int[chars.length];
-       for (int i = chars.length-2; i >= 0; i--) {
-           int numericValue = Character.getNumericValue(chars[i]);
-           result = result * 2;
-           if (numericValue==1) {
-               arr[i] =  result;
-           }
-           }
-            int sum = 0;
-       for (int i = 0; i < arr.length; i++) {
-            sum =  sum + arr[i] ;
-       }
-        return sum+1;
-    }
+       return n & ~(1 << (k - 1));
+   }
+
+
+
 
 //    public static void main(String[] args) {
 //        int solution = CodeSignal.solution(2147483647, 16);
